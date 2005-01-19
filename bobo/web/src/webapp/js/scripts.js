ac628ff32a807ba23040f7e21fb5ca432aa0e7f3
romale <romale>
2005-01-19 01:08:35 +0000

    Ajout de la fonction setDefaultFocus() à implémenter

--- bobo/web/src/webapp/js/scripts.js
+++ bobo/web/src/webapp/js/scripts.js
@@ -1,18 +1,20 @@
 /*
- * $Id: scripts.js,v 1.1 2005/01/18 09:26:18 romale Exp $
+ * $Id: scripts.js,v 1.2 2005/01/19 01:08:35 romale Exp $
  */
 
 function init() {
     initMenu();
+    setDefaultFocus();
 }
+
 
 // http://www.alistapart.com/articles/horizdropdowns/
 function initMenu() {
     // cette fonction concerne uniquement IE
     if (document.all && document.getElementById) {
-        navRoot = document.getElementById("menu");
-        liChildren = navRoot.getElementsByTagName("li");
-        for (i = 0; i < liChildren.length; i++) {
+        var navRoot = document.getElementById("menu");
+        var liChildren = navRoot.getElementsByTagName("li");
+        for (var i = 0; i < liChildren.length; ++i) {
             node = liChildren[i];
             node.onmouseover = function() {
                 this.className += " over";
@@ -22,4 +24,23 @@ function initMenu() {
             }
         }
     }
+}
+
+
+function setDefaultFocus() {
+// TODO corriger les erreurs de syntaxe
+/*
+    var forms = document.getElementsByTagName("form");
+    for(var i = 0; i < forms.length; ++i) {
+        var form = forms[i];
+        var children = form.childNodes;
+        for(var j = 0; j < children.length; ++j) {
+            var elem = children[j];
+            var elemClass = elem.className;
+            if(elemClass && elemClass.indexOf("default-focus") != -1) {
+                elem.focus();
+            }
+        }
+    }
+*/
 }
