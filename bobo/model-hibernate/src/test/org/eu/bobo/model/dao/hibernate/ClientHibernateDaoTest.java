6ea11745fdc1e226b2b9f38c56fbb7d0213b8747
romale <romale>
2005-01-21 10:14:31 +0000

    Nettoyage

--- bobo/model-hibernate/src/test/org/eu/bobo/model/dao/hibernate/ClientHibernateDaoTest.java
+++ bobo/model-hibernate/src/test/org/eu/bobo/model/dao/hibernate/ClientHibernateDaoTest.java
@@ -1,13 +1,36 @@
-/*                                                   *\
- * Copyright © 2004 Centre Informatique Multimédia   *
- *                                                   *
- * $Id: ClientHibernateDaoTest.java,v 1.1 2005/01/13 13:38:06 romale Exp $
-\*                                                   */
+/*
+ * Copyright (c) 2005, Bobo team
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of the Bobo project nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 
 package org.eu.bobo.model.dao.hibernate;
-
-import java.util.List;
 
 import org.eu.bobo.model.Identite;
 import org.eu.bobo.model.bo.Adresse;
@@ -16,17 +39,19 @@ import org.eu.bobo.model.bo.Client;
 import org.eu.bobo.model.bo.Telephone;
 import org.eu.bobo.model.dao.ClientDao;
 
+import java.util.List;
+
 
 /**
  * DOCUMENT ME!
  *
  * @author alex
- * @version $Revision: 1.1 $, $Date: 2005/01/13 13:38:06 $
+ * @version $Revision: 1.2 $, $Date: 2005/01/21 10:14:31 $
  */
 public class ClientHibernateDaoTest extends AbstractHibernateDaoTest {
     //~ Champs d'instance ------------------------------------------------------
 
-    private ClientDao             clientDao;
+    private ClientDao clientDao;
 
     //~ Méthodes ---------------------------------------------------------------
 
@@ -190,12 +215,12 @@ public class ClientHibernateDaoTest extends AbstractHibernateDaoTest {
 
     protected void setUp() throws Exception {
         super.setUp();
-        clientDao          = (ClientDao) getBean("clientDao");
+        clientDao = (ClientDao) getBean("clientDao");
     }
 
 
     protected void tearDown() throws Exception {
-        clientDao          = null;
+        clientDao = null;
         super.tearDown();
     }
 }
--- bobo/model-hibernate/src/test/org/eu/bobo/model/dao/hibernate/UtilisateurHibernateDaoTest.java
+++ bobo/model-hibernate/src/test/org/eu/bobo/model/dao/hibernate/UtilisateurHibernateDaoTest.java
@@ -1,28 +1,51 @@
-/*                                                   *\
- * Copyright © 2004 Centre Informatique Multimédia   *
- *                                                   *
- * $Id: UtilisateurHibernateDaoTest.java,v 1.1 2005/01/13 13:38:06 romale Exp $
-\*                                                   */
+/*
+ * Copyright (c) 2005, Bobo team
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of the Bobo project nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 
 package org.eu.bobo.model.dao.hibernate;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-import net.sf.acegisecurity.providers.dao.AuthenticationDao;
-
 import org.eu.bobo.model.bo.Autorite;
 import org.eu.bobo.model.bo.Utilisateur;
 import org.eu.bobo.model.dao.UtilisateurDao;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 
 /**
  * DOCUMENT ME!
  *
  * @author alex
- * @version $Revision: 1.1 $, $Date: 2005/01/13 13:38:06 $
+ * @version $Revision: 1.2 $, $Date: 2005/01/21 10:14:31 $
  */
 public class UtilisateurHibernateDaoTest extends AbstractHibernateDaoTest {
     //~ Champs d'instance ------------------------------------------------------
@@ -93,7 +116,7 @@ public class UtilisateurHibernateDaoTest extends AbstractHibernateDaoTest {
 
 
     public void testDeleteUserAvecProprietes() {
-        final String      login = "julien";
+        final String      login  = "julien";
         final Utilisateur julien = utilisateurDao.findByLogin(login);
         assertNotNull(julien);
         assertFalse(julien.getProprietes().isEmpty());
@@ -231,10 +254,5 @@ public class UtilisateurHibernateDaoTest extends AbstractHibernateDaoTest {
     protected void tearDown() throws Exception {
         utilisateurDao = null;
         super.tearDown();
-    }
-    
-    
-    private AuthenticationDao getAuthenticationDao() {
-        return (AuthenticationDao) getBean("utilisateurDaoOriginal");
     }
 }
